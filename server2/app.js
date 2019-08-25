f4e98bb2293e785866953eb69a79cd7bc88a76f6
bryanmej <bryanmej@gmail.com>
2019-08-25 00:20:05 -0500

    removed dead code

--- server2/app.js
+++ server2/app.js
@@ -3,7 +3,6 @@ require("dotenv").config();
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const express = require("express");
-const favicon = require("serve-favicon");
 const hbs = require("hbs");
 const mongoose = require("mongoose");
 const logger = require("morgan");
@@ -66,7 +65,6 @@ app.use(cookieParser());
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "hbs");
 app.use(express.static(path.join(__dirname, "public")));
-//app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
 // default value for title local
 app.locals.title = "Express - Generated with IronGenerator";
